4c8e45b20bb27bf37f688de33c1ec3a36037a8be
meghpatil21 <megh.after18@gmail.com>
2022-09-05 16:13:34 +0530

    [megh] compare 2 method using encapsulation

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,7 +9,7 @@ namespace ConsoleApp1
             Console.WriteLine("Welcome to Line Comparison Computation.");
             Console.WriteLine();
 
-            Console.WriteLine("Select option from following: \n1.Lengh of Line \n2.Check equality of two lines \n3.Compare two lines");
+            Console.WriteLine("Select option from following: \n1.Lengh of Line \n2.Check equality of two lines \n3.Compare two lines \n4.compare using oops");
             int option = Convert.ToInt32(Console.ReadLine());
 
             switch (option)
