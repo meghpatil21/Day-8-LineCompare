95856266c0c1625b33cc0c136adca331e78392df
meghpatil21 <megh.after18@gmail.com>
2022-09-05 13:34:29 +0530

    [megh] Line legnth calculation added

---
+++ ConsoleApp1/LineCompare.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class LineCompare
+    {
+        public static void LengthLine()
+        {
+            Console.WriteLine("Enter X Co-ordinates of first point: ");
+            Double x1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Y Co-ordinates of first point: ");
+            Double y1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter X Co-ordinates of second point: ");
+            Double x2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter X Co-ordinates of second point: ");
+            Double y2 = Convert.ToDouble(Console.ReadLine());
+
+            double x = (x2 - x1) * (x2 - x1);
+            double y = (y2 - y1) * (y2 - y1);
+
+            double lengthOfLine = Math.Sqrt(x + y);
+            lengthOfLine = System.Math.Round(lengthOfLine, 2);
+            Console.WriteLine("Lengh of line is " + lengthOfLine);
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,6 +5,20 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Computation.");
+            Console.WriteLine();
+
+            Console.WriteLine("Select option from following: \n1.Lengh of Line");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    LineCompare.LengthLine();
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice");
+                    break;
+            }
         }
     }
 }
